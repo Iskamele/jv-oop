0fea14fc9f5bfbf71a4009aa0d6f1e3560d4d60d
username <username@domain.com>
2022-10-24 21:51:32 +0300

    Added new classes inherited from Machine. Inside those classes realized override methods doWork and stopWord as part of the assignment: a print message about change status objects. Fixed mistakes in sout

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Truck extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Truck is started");
+        System.out.println("Truck started work");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Truck is stopped");
+        System.out.println("Truck stopped work");
     }
 }
